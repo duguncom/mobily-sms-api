fed017c7b5c9d050c4d64c15123815a86fdcb632
Farhad Safarov <farhad.safarov@gmail.com>
2019-07-26 11:59:27 +0300

    fix class case mismatch

--- requests/getBalance.php
+++ requests/getBalance.php
@@ -1,81 +1,84 @@
 <?php namespace MobilyAPI\requests;
 
-	use MobilyAPI\Request;
+use MobilyAPI\Request;
 
-	/**
-	 * A request to check for the current balance
-	 * @package MobilyAPI
-	 */
-	class GetBalance extends Request{
-		/**
-		 * @var string
-		 */
-		public $function = 'balance';
-		/**
-		 * @var array
-		 */
-		public $responseCodes = [
-			0 => 'connection_failed',
-			1 => 'invalid_username',
-			2 => 'invalid_password'
-		];
+    /**
+     * A request to check for the current balance
+     * @package MobilyAPI
+     */
+    class GetBalance extends Request
+    {
+        /**
+         * @var string
+         */
+        public $function = 'balance';
+        /**
+         * @var array
+         */
+        public $responseCodes = [
+            0 => 'connection_failed',
+            1 => 'invalid_username',
+            2 => 'invalid_password'
+        ];
 
-		/**
-		 * The balance as an array
-		 * @return array|bool
-		 */
-		public function balance(){
-			// The response
-			$response = $this->getResponse();
+        /**
+         * The balance as an array
+         * @return array|bool
+         */
+        public function balance()
+        {
+            // The response
+            $response = $this->getResponse();
 
-			// The body
-			$balance = explode('/', $response->raw());
+            // The body
+            $balance = explode('/', $response->raw());
 
-			if(count($balance) != 2){
-				return false;
-			}
+            if (count($balance) != 2) {
+                return false;
+            }
 
-			return [
-				'remaining' => $balance[1],
-				'total' => $balance[0]
-			];
-		}
+            return [
+                'remaining' => $balance[1],
+                'total' => $balance[0]
+            ];
+        }
 
-		/**
-		 * The remaining balance
-		 * @return int|null
-		 */
-		public function remaining(){
-			$balance = $this->balance();
+        /**
+         * The remaining balance
+         * @return int|null
+         */
+        public function remaining()
+        {
+            $balance = $this->balance();
 
-			if(is_array($balance)){
-				return $balance['remaining'];
+            if (is_array($balance)) {
+                return $balance['remaining'];
+            } else {
+                return false;
+            }
+        }
 
-			}else{
-				return false;
-			}
-		}
+        /**
+         * The total balance
+         * @return int|false
+         */
+        public function total()
+        {
+            $balance = $this->balance();
 
-		/**
-		 * The total balance
-		 * @return int|false
-		 */
-		public function total(){
-			$balance = $this->balance();
+            if (is_array($balance)) {
+                return $balance['total'];
+            } else {
+                return false;
+            }
+        }
 
-			if(is_array($balance)){
-				return $balance['total'];
-
-			}else{
-				return false;
-			}
-		}
-
-		/**
-		 * Tells if there is credit available
-		 * @return bool
-		 */
-		public function available(){
-			return (int) $this->remaining() > 0;
-		}
-	}
+        /**
+         * Tells if there is credit available
+         * @return bool
+         */
+        public function available()
+        {
+            return (int) $this->remaining() > 0;
+        }
+    }
--- requests/sendingStatus.php
+++ requests/sendingStatus.php
@@ -1,32 +1,34 @@
 <?php namespace MobilyAPI\requests;
 
-	use MobilyAPI\Request;
+use MobilyAPI\Request;
 
-	/**
-	 * Checks sending status
-	 * @package MobilyAPI\requests
-	 */
-	class SendingStatus extends Request{
-		/**
-		 * @var string
-		 */
-		public $function = 'sendStatus';
-		/**
-		 * @var array
-		 */
-		public $responseCodes = [
-			1 => 'available'
-		];
-		/**
-		 * @var array
-		 */
-		public $successResponseCode = [1];
+    /**
+     * Checks sending status
+     * @package MobilyAPI\requests
+     */
+    class SendingStatus extends Request
+    {
+        /**
+         * @var string
+         */
+        public $function = 'sendStatus';
+        /**
+         * @var array
+         */
+        public $responseCodes = [
+            1 => 'available'
+        ];
+        /**
+         * @var array
+         */
+        public $successResponseCode = [1];
 
-		/**
-		 * Tells if it's okay to send right now
-		 * @return bool
-		 */
-		public function available(){
-			return $this->getResponse()->raw() == 1;
-		}
-	}
+        /**
+         * Tells if it's okay to send right now
+         * @return bool
+         */
+        public function available()
+        {
+            return $this->getResponse()->raw() == 1;
+        }
+    }
